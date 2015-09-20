20bb0365b267aed097c3b6fcebb35def25fafd69
Rahul Sharma <rsconceptx@gmail.com>
2015-09-21 00:39:15 +0530

    trying echo command

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
 // A terminal shell implementation in Rust 
 use std::io;
 
+
 fn main() {
 
-		let mut cmd = String::new();
-		println!("{}::","\u{019B}");
-		io::stdin().read_line(&mut cmd)
+	let mut cmd = String::new();
+	loop {
+
+	println!("{}::","\u{019B}");
+	io::stdin().read_line(&mut cmd)
 		.ok()
 		.expect("Error: Command Not Found");
-		let cmd_slice = &cmd;
-		
-	
+	let cmd_vec:Vec<&str> = cmd.split(" ").collect();
+	match cmd_vec[0] {
+		"echo" => println!("{}",cmd_vec[1]),
+		_ => println!("Error: Command not found"),
+	}
+
+	}
 }
